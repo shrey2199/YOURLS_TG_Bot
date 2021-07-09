0a1916f9d9305177ea18a425fc57d5ae3f37089d
shrey2199 <52773351+shrey2199@users.noreply.github.com>
2021-07-09 10:46:59 +0530

    Add delete command, enable URL encoding

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import telebot
 import requests
 import json
 import logging
+import urllib.parse
 from functools import wraps
 from random import *
 
@@ -43,28 +44,44 @@ def restricted(func):
 @bot.message_handler(commands=['start'])
 @restricted
 def start(m):
-    bot.send_message(m.chat.id, text="Hey !! Welcome to Shrey23 URL Shorten Bot !\n\n<b>To Shorten your URLs :-</b>\n\nSend /short <code>&lt;link&gt;</code> to the bot.\n\nSend /help for More Info !", parse_mode=telegram.ParseMode.HTML)
+    bot.send_message(m.chat.id, text="""Hey !! Welcome to Shrey23 URL Shorten Bot !
+    \n<b>To Shorten your URLs :-</b>
+    Send /short <code>&lt;link&gt;</code> to the bot.
+    \nSend /help for More Info !""", parse_mode=telegram.ParseMode.HTML)
 
 @bot.message_handler(commands=['help'])
 @restricted
 def help(m):
-	bot.send_message(chat_id=m.chat.id, text="This Bot can Shorten Your URLs with support for Custom Keywords.\n\n<b>For using Random Keyword :-</b>\n\nSend /short <code>&lt;link&gt;</code>\n\n<b>For using Custom Keyword :-</b>\n\nSend /short <code>&lt;link&gt;</code> <code>&lt;keyword&gt;</code>\n\n<b>To get Link Info :-</b>\n\nSend /info <code>&lt;keyword/link&gt;</code>", parse_mode=telegram.ParseMode.HTML)
+	bot.send_message(chat_id=m.chat.id, text="""This Bot can Shorten Your URLs with support for Custom Keywords.
+    \n<b>For using Random Keyword :-</b>
+    Send /short <code>&lt;link&gt;</code>
+    \n<b>For using Custom Keyword :-</b>
+    Send /short <code>&lt;link&gt;</code> <code>&lt;keyword&gt;</code>
+    \n<b>To get Link Info :-</b>
+    Send /info <code>&lt;keyword/link&gt;</code>
+    \n<b>To Delete A Link :-</b>
+    Send /delete <code>&lt;keyword/link&gt;</code>""", parse_mode=telegram.ParseMode.HTML)
 
 @bot.message_handler(commands=['short'])
 @restricted
 def short(m):
     chat = m.text[9:]
     if chat == "":
-        bot.send_message(chat_id=m.chat.id, text="Pls Send the Command with Valid Queries !!\n\n<b>For using Random Keyword :-</b>\n\nSend /short <code>&lt;link&gt;</code>\n\n<b>For using Custom Keyword :-</b>\n\nSend /short <code>&lt;link&gt;</code> <code>&lt;keyword&gt;</code>", parse_mode=telegram.ParseMode.HTML)
+        bot.send_message(chat_id=m.chat.id, text="""Pls Send the Command with Valid Queries !!
+        \n<b>For using Random Keyword :-</b>
+        Send /short <code>&lt;link&gt;</code>
+        \n<b>For using Custom Keyword :-</b>
+        Send /short <code>&lt;link&gt;</code> <code>&lt;keyword&gt;</code>""", parse_mode=telegram.ParseMode.HTML)
     else:
 
         link = m.text.split()[1]
+        elink = urllib.parse.quote(link)
         allchar = "abcdefghijklmnopqrstuvwxyz0123456789"
         if len(m.text.split()) == 3:
         	word = m.text.split()[2]
         if len(m.text.split()) == 2:
         	word = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
-        url = 'https://'+YL_DOMAIN+'/yourls-api.php?signature='+YL_SIG+'&action=shorturl&url=' + link + '&keyword=' + word + '&format=json'
+        url = 'https://'+YL_DOMAIN+'/yourls-api.php?signature='+YL_SIG+'&action=shorturl&url=' + elink + '&keyword=' + word + '&format=json'
 
         try:
             r = requests.get(url)
@@ -91,10 +108,13 @@ def short(m):
 def info(m):
     chat = m.text[5:]
     if chat == "":
-        bot.send_message(m.chat.id, text = "Pls Send the Command with Valid Queries !!\n\n<b>To get Link Info :-</b>\n\nSend /info <code>&lt;keyword/link&gt;</code>", parse_mode=telegram.ParseMode.HTML)
+        bot.send_message(m.chat.id, text = """Pls Send the Command with Valid Queries !!
+        \n<b>To get Link Info :-</b>
+        Send /info <code>&lt;keyword/link&gt;</code>""", parse_mode=telegram.ParseMode.HTML)
     else:
         link = m.text.split()[1]
-        url = 'https://'+YL_DOMAIN+'/yourls-api.php?signature='+YL_SIG+'&action=url-stats&shorturl=' + link + '&format=json'
+        elink = urllib.parse.quote(link, safe='')
+        url = 'https://'+YL_DOMAIN+'/yourls-api.php?signature='+YL_SIG+'&action=url-stats&shorturl=' + elink + '&format=json'
         try:
             r = requests.get(url)
             res = r.json()
@@ -104,8 +124,36 @@ def info(m):
                 "\n*Created :\t\t*" + str(res["link"]["timestamp"]) +
                 "\n*Clicks :\t\t*" + str(res["link"]["clicks"]), parse_mode='Markdown')
                 print("RESPONSE: " + str(res))
+            if res["statusCode"] == 404:
+                bot.send_message(m.chat.id, "*Error :\t\t*Short URL not found.", parse_mode='Markdown')
             else:
-                bot.send_message(m.chat.id, "*Error :\t\t*" + str(res["message"]), parse_mode='Markdown')
+                bot.send_message(m.chat.id, str(res["message"]), parse_mode='Markdown')
         except json.JSONDecodeError:
             bot.send_message(m.chat.id, "*Error :\t\t*YOURLS SERVER NOT REACHABLE; TRY AGAIN LATER", parse_mode='Markdown')
+
+@bot.message_handler(commands=['delete'])
+@restricted
+def info(m):
+    chat = m.text[7:]
+    if chat == "":
+        bot.send_message(m.chat.id, text = """Pls Send the Command with Valid Queries !!
+        \n<b>To Delete A Link :-</b>
+        Send /delete <code>&lt;keyword/link&gt;</code>""", parse_mode=telegram.ParseMode.HTML)
+    else:
+        link = m.text.split()[1]
+        elink = urllib.parse.quote(link, safe='')
+        url = 'https://'+YL_DOMAIN+'/yourls-api.php?signature='+YL_SIG+'&action=delete&shorturl=' + elink + '&format=json'
+        try:
+            r = requests.get(url)
+            res = r.json()
+            if res["statusCode"] == 200:
+                bot.send_message(m.chat.id, "The <b>link/keyword</b>: <code>"+link+"</code> has been successfully deleted.", parse_mode=telegram.ParseMode.HTML)
+            if res["statusCode"] == 404:
+                bot.send_message(m.chat.id, "*Error :\t\t*Short URL not found.", parse_mode='Markdown')
+            else:
+                bot.send_message(m.chat.id, str(res["message"]), parse_mode='Markdown')
+        except json.JSONDecodeError:
+            bot.send_message(m.chat.id, "*Error :\t\t*YOURLS SERVER NOT REACHABLE; TRY AGAIN LATER", parse_mode='Markdown')
+
+
 bot.polling(none_stop=True, timeout=3600)
